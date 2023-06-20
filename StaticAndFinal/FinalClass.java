677930d7a76e2411c2dd0c949ba80366242fbc9b
14343 <manoharreddyyatelli@gmail.com>
2023-06-19 22:32:30 -0500

    done

---
+++ StaticAndFinal/FinalClass.java
@@ -0,0 +1,10 @@
+package StaticAndFinal.src;
+
+final class FinalClass {
+    private final int finalVariable = 10;
+
+    final void finalMethod() {
+        System.out.println("Final method");
+    }
+
+}
