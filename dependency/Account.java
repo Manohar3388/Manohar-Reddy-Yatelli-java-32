1f9a568f8829fd85d9c6657eaf3cadf684d3df25
14343 <manoharreddyyatelli@gmail.com>
2023-07-06 15:48:17 -0500

    done

---
+++ dependency/Account.java
@@ -0,0 +1,28 @@
+package dependency;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Account {
+
+    public String getAccountName() {
+        return AccountName;
+    }
+
+    public void setAccountName(String accountName) {
+        AccountName = accountName;
+    }
+
+    private String AccountName;
+
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        AccountNumber = accountNumber;
+    }
+
+    private String AccountNumber;
+
+}
